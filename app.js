752a258ec19e38b1be15008698e49fd110f882e1
B10756052 <B10756052@gmail.com>
2022-06-24 15:13:19 +0800

    基本html、scss、js設定

---
+++ app.js
@@ -0,0 +1,28 @@
+// -------------------------------------------------------------------------------
+// class寫法
+
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     sayhi() {
+//         console.log('hi');
+//     }
+
+// }
+
+// class Student extends Person {
+//     constructor(name, age, major) {
+//         super(name, age);
+//         this.major = major;
+//     }
+// }
+
+// let Jay = new Person('jj Su', 21);
+// let lin = new Student('linlin', 19, "MIS");
+
+// console.log(lin);
+
+// lin.sayhi();
+// -------------------------------------------------------------------------------
